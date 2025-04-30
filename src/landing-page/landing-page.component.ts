cc26c67050757e0c1848f75f74a6cfdccdcc2429
Fernando Rivero de Miranda <EX2025300495@nfoque.local>
2025-04-30 14:37:00 +0200

    ADD - Added new animation to scroll icon, fixed its position on website and added scroll counter

--- src/landing-page/landing-page.component.ts
+++ src/landing-page/landing-page.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'landing-page',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
 export class LandingPageComponent {
+  scrollCounter: number
+  @ViewChild('pageContainer') webPageContainer!: ElementRef;
+
   constructor() {
+    this.scrollCounter = 0;
     console.log("HA ENTRADO EN EL LANDING PAGE")
   }
+
+  ngAfterViewInit() {
+    this.webPageContainer.nativeElement.addEventListener('wheel', (event: WheelEvent)=> {
+      if(event.deltaY < 0) {
+        this.scrollCounter++;
+      } else if (event.deltaY > 0 && this.scrollCounter > 0) {
+        this.scrollCounter--;
+      }
+      console.log(this.scrollCounter) 
+    });
+  }
+
 }
